1178a5023c3d26907785d032ff60015af33cbf3a
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-18 22:58:07 -0500

    app-demo-backend project with Maven setup, including .gitignore, .gitattributes, and Docker Compose configuration. Add Spring Boot application structure with necessary dependencies and configuration files.

---
+++ app-demo-backend/src/main/java/com/isai/app_demo_backend/AppDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.isai.app_demo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDemoBackendApplication.class, args);
+	}
+
+}
---
+++ app-demo-backend/src/test/java/com/isai/app_demo_backend/AppDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.app_demo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
