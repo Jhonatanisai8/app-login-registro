2373ff728a7b145d3f22afae86c8ee9e633edab2
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-18 23:43:27 -0500

    Enhance SecurityConfig class with additional security filters and update pom.xml for dependency management improvements.

---
+++ app-demo-backend/src/main/java/com/isai/app_demo_backend/dtos/ReqRes.java
@@ -0,0 +1,29 @@
+package com.isai.app_demo_backend.dtos;
+
+import com.isai.app_demo_backend.entitys.UsuarioEntidad;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReqRes {
+  private Integer codigoEstado;
+  private String error;
+  private String mensaje;
+  private String token;
+  private String refreshToken;
+  private String horaDeVencimiento; //expirationTime
+  private String nombre;
+  private String ciudad;
+  private String rol;
+  private String email;
+  private String password;
+  private UsuarioEntidad usuario;
+  private List<UsuarioEntidad> listaUsuarios;
+}
