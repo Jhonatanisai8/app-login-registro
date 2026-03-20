ccf3f1125f34854197be2815440fddb88f465827
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-19 23:28:33 -0500

    feat: implement initial Spring Security configuration with JWT authentication and role-based authorization.

--- app-demo-backend/src/main/java/com/isai/app_demo_backend/config/SecurityConfig.java
+++ app-demo-backend/src/main/java/com/isai/app_demo_backend/config/SecurityConfig.java
@@ -1,10 +1,66 @@
 package com.isai.app_demo_backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.isai.app_demo_backend.config.jwt.JWTAuthFilter;
+import com.isai.app_demo_backend.services.UsuarioDetailsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-  
+
+  private final UsuarioDetailsService usuarioDetailsService;
+
+  private final JWTAuthFilter jwtAuthFilter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**").permitAll())
+        .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/public/**").permitAll()
+            .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+            .requestMatchers("/api/v1/user/**").hasRole("USER")
+            .requestMatchers("/api/v1/adminuser/**").hasAnyAuthority("ADMIN", "USER")
+            .anyRequest().authenticated())
+        .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider())
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(usuarioDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+      throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
 }
