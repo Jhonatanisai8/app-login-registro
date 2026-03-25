b397a738e70aa641bc04604ba384f253bcc5929c
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-24 23:22:32 -0500

    feat: implementacion de metodo en el controlador de auth para refrescar el token

--- app-demo-backend/src/main/java/com/isai/app_demo_backend/controllers/UsuarioController.java
+++ app-demo-backend/src/main/java/com/isai/app_demo_backend/controllers/UsuarioController.java
@@ -4,13 +4,10 @@ import com.isai.app_demo_backend.dtos.ReqRes;
 import com.isai.app_demo_backend.services.UserManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api/v1/usuarios")
+@RequestMapping(path = "/api/v1")
 @RequiredArgsConstructor
 public class UsuarioController {
   private final UserManagementService userManagementService;
@@ -20,8 +17,13 @@ public class UsuarioController {
     return ResponseEntity.ok(userManagementService.registrar(registrarRequest));
   }
 
-  @RequestMapping(method = RequestMethod.GET)
+  @RequestMapping(path = "/admin/usuarios", method = RequestMethod.GET)
   public ResponseEntity<ReqRes> obtenerUsuarios() {
     return ResponseEntity.ok(userManagementService.obtenerUsuarios());
+  }
+
+  @RequestMapping(path = "/admin/usuarios{idUsuario}", method = RequestMethod.GET)
+  public ResponseEntity<ReqRes> obtenerUsuario(@PathVariable Long idUsuario) {
+    return ResponseEntity.ok(userManagementService.obtenerUsuarioID(idUsuario));
   }
 }
