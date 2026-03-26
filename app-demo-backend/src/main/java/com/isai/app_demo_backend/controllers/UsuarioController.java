2f349fa3652a526422cb00713f771eb1812147c5
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-25 21:48:10 -0500

    feat: implementacion del metodo para obtener la informacion del usuario logueado

--- app-demo-backend/src/main/java/com/isai/app_demo_backend/controllers/UsuarioController.java
+++ app-demo-backend/src/main/java/com/isai/app_demo_backend/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import com.isai.app_demo_backend.dtos.ReqRes;
 import com.isai.app_demo_backend.services.UserManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,5 +32,13 @@ public class UsuarioController {
   @RequestMapping(path = "/admin/usuarios{idUsuario}", method = RequestMethod.PUT)
   public ResponseEntity<ReqRes> actualizarUsuario(@PathVariable Long idUsuario, @RequestBody ReqRes actualizarUsuarioRequest) {
     return ResponseEntity.ok(userManagementService.actualizarUsuarioID(idUsuario, actualizarUsuarioRequest));
+  }
+
+  @RequestMapping(path = "/adminuser/obtener-perfil", method = RequestMethod.PUT)
+  public ResponseEntity<ReqRes> obtenerPerfil() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String email = authentication.getName();
+    ReqRes response = userManagementService.obtenerInformacionUsuarioPorEmail(email);
+    return ResponseEntity.status(response.getCodigoEstado()).body(response);
   }
 }
